a6a07fc1dab2c12ed95b1a64aa687534aa0f7f96
PrakornChaikure <6206022610050@fitm.kmutnb.ac.th>
2022-02-16 20:46:28 +0700

    Final

--- testapp4/lib/main.dart
+++ testapp4/lib/main.dart
@@ -1,135 +1,180 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'foodMenu.dart';
-import 'MoneyBox.dart';
+import 'package:flutter/gestures.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "My App",
-        home: MyHomePage(),
-        theme: ThemeData(primarySwatch: Colors.blueGrey));
+      home: Home(),
+    );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<foodMenu> menu = [
-    foodMenu("ผัดพริกป่นราดข้าว", "40", "assets/images/1.jpg"),
-    foodMenu("ข้าวผัดไข่หมูแดดเดียว", "50", "assets/images/7.jpg"),
-    foodMenu("ผัดไข่ข้าว", "55", "assets/images/2.jpg"),
-    foodMenu("หมูแดดผัดข้าว", "60", "assets/images/7.jpg"),
-    foodMenu("ข้าวผัดหมูแดด", "65", "assets/images/7.jpg"),
-    foodMenu("กุ้งชุบแป้งทอด", "70", "assets/images/6.jpg"),
-    foodMenu("ปูชุบแป้งทอด", "75", "assets/images/3.jpg"),
-    foodMenu("ปลาชุบแป้งทอด", "80", "assets/images/4.jpg"),
-    foodMenu("ทอดปลา", "85", "assets/images/5.jpg"),
-    foodMenu("ปลาดุกทอด", "90", "assets/images/8.jpg"),
-  ];
+class _HomeState extends State<Home> {
+  TextEditingController username = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController cnfpassword = TextEditingController();
+  TextEditingController mobile = TextEditingController();
+
+  @override
+  void initState() {
+    username.text = ""; //innitail value of text field
+    email.text = "";
+    password.text = "";
+    cnfpassword.text = "";
+    mobile.text = "";
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(
-            "Amount Balance",
-            style: TextStyle(fontSize: 30, color: Colors.grey),
-          ),
+          title: Text("REGISTER"),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                height: 100,
-                padding: const EdgeInsets.all(20.0),
-                decoration: BoxDecoration(
-                    color: Colors.blue.shade200,
-                    borderRadius: BorderRadius.circular(20)),
-                child: InputDecoratorExample(),
+        body: Container(
+            constraints: BoxConstraints.expand(),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/wall.jpg"),
+                    fit: BoxFit.cover)),
+            padding: EdgeInsets.all(20),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/Nice.jpg",
+                    height: 120,
+                  ),
+                  TextField(
+                      controller: username,
+                      decoration: InputDecoration(
+                        labelText: "Username",
+                        labelStyle: const TextStyle(color: Colors.white),
+                        hintText: 'Enter you Username',
+                        hintStyle: const TextStyle(color: Colors.white),
+                        prefixIcon: Icon(Icons.person, color: Colors.white),
+                        border: myinputborder(),
+                        enabledBorder: myinputborder(),
+                        focusedBorder: myfocusborder(),
+                      )),
+                  Container(height: 8),
+                  TextField(
+                      controller: email,
+                      decoration: InputDecoration(
+                        labelText: "Email Address",
+                        labelStyle: const TextStyle(color: Colors.white),
+                        hintText: 'Enter you Address',
+                        hintStyle: const TextStyle(color: Colors.white),
+                        prefixIcon: Icon(Icons.email, color: Colors.white),
+                        border: myinputborder(),
+                        enabledBorder: myinputborder(),
+                        focusedBorder: myfocusborder(),
+                      )),
+                  Container(height: 8),
+                  TextField(
+                      controller: password,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: const TextStyle(color: Colors.white),
+                        hintText: 'Enter you Password',
+                        hintStyle: const TextStyle(color: Colors.white),
+                        prefixIcon: Icon(Icons.lock, color: Colors.white),
+                        border: myinputborder(),
+                        enabledBorder: myinputborder(),
+                        focusedBorder: myfocusborder(),
+                      )),
+                  Container(height: 8),
+                  TextField(
+                      controller: cnfpassword,
+                      decoration: InputDecoration(
+                        labelText: "Confirm Password",
+                        labelStyle: const TextStyle(color: Colors.white),
+                        hintText: 'Enter you Password',
+                        hintStyle: const TextStyle(color: Colors.white),
+                        prefixIcon: Icon(Icons.lock, color: Colors.white),
+                        border: myinputborder(),
+                        enabledBorder: myinputborder(),
+                        focusedBorder: myfocusborder(),
+                      )),
+                  Container(height: 8),
+                  TextField(
+                      controller: mobile,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.phone, color: Colors.white),
+                        labelText: "Mobile",
+                        labelStyle: const TextStyle(color: Colors.white),
+                        hintText: 'Enter you Number',
+                        hintStyle: const TextStyle(color: Colors.white),
+                        enabledBorder: myinputborder(),
+                        focusedBorder: myfocusborder(),
+                      )),
+                  Container(
+                    padding: const EdgeInsets.all(10.30),
+                    child: TextButton(
+                      child: Text(
+                        "REGISTER",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      style: TextButton.styleFrom(
+                        primary: Colors.black,
+                        backgroundColor: Colors.white,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      " Already have an Account ?",
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "Login  ",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              MoneyBox("ยอดคงเหลือ", 30000.512, 80, Colors.grey.shade300),
-              SizedBox(height: 3),
-              MoneyBox("รายรับ", 10000, 80, Colors.grey.shade400),
-              SizedBox(height: 3),
-              MoneyBox("รายจ่าย", 80000, 80, Colors.grey.shade600),
-              SizedBox(height: 3),
-              MoneyBox("ค้างจ่าย", 4000, 80, Colors.grey.shade800),
-              Container(
-                child: TextButton(
-                  child: Text(
-                    "BUTTON",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.green,
-                  ),
-                  onPressed: () {},
-                ),
-              )
-            ],
-          ),
+            )));
+  }
+
+  OutlineInputBorder myinputborder() {
+    //return type is OutlineInputBorder
+    return OutlineInputBorder(
+        //Outline border type for TextFeild
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        borderSide: BorderSide(
+          color: Colors.white,
+          width: 3,
         ));
   }
 
-  List<Widget> getData(int count) {
-    List<Widget> data = [];
-
-    data.add(
-      Text(
-        "กดปุ่ม เพื่อค่า ตัวเลข",
-        style: TextStyle(fontSize: 40, color: Colors.red),
-      ),
-    );
-    for (var i = 1; i <= 10; i++) {
-      var menu = ListTile(
-        title: Text(
-          "เมนูที่ $i",
-          style: TextStyle(fontSize: 20, color: Colors.red),
-        ),
-        subtitle: Text("หัวข้อย่อยที่ $i"),
-      );
-      data.add(menu);
-    }
-    return data;
+  OutlineInputBorder myfocusborder() {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        borderSide: BorderSide(
+          color: Colors.white,
+          width: 3,
+        ));
   }
-}
-
-class InputDecoratorExample extends StatelessWidget {
-  const InputDecoratorExample({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        border: const OutlineInputBorder(),
-        labelText: 'Account Name',
-        labelStyle:
-            MaterialStateTextStyle.resolveWith((Set<MaterialState> states) {
-          return TextStyle(letterSpacing: 1.3);
-        }),
-      ),
-      validator: (String? value) {
-        if (value == null || value == '') {
-          return 'Enter name';
-        }
-        return null;
-      },
-      autovalidateMode: AutovalidateMode.always,
-    );
-  }
+  //create a function like this so that you can use it wherever you want
 }
